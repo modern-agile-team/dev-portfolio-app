c8947f28b998b832917a10bb807744dd84b3770a
seohyunsim <ssh4563@naver.com>
2022-08-07 23:34:20 +0900

    [Feat] create portfolio demo-app from seohyunsim (#7)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,5 @@
 import {
   Header,
-  TechStackInput,
   TechStackList,
   Contact,
   Intro,
@@ -12,95 +11,166 @@ import {
   Carousel,
   Experience,
 } from "dev-portfolio";
+import {
+  logoOptionDefault,
+  channelOptionDefault,
+  sideBarOptionDefault,
+} from "./defaultProps/HeaderProps";
+import {
+  techStacksDefault,
+  historyListDefault,
+  introOptionDefault,
+  contactOptionDefault,
+} from "./defaultProps/CommonProps";
 import styled from "styled-components";
 import React from "react";
 import "./App.css";
 
 function App() {
-  const techStacksDefault = [
-    {
-      nameOption: { name: "Javascript", size: "40px", iconColor: "#E2D784" },
-      progressBarOption: {
-        rate: "45%",
-        colorTo: "#E2D784",
-        colorFrom: "whitesmoke",
-      },
-    },
-    {
-      nameOption: { name: "HTML-5", size: "50px", iconColor: "#E34F26" },
-      progressBarOption: {
-        rate: "30%",
-        colorTo: "#E34F26",
-        colorFrom: "whitesmoke",
-      },
-    },
-    {
-      nameOption: { name: "Nodejs", size: "40px", iconColor: "#339933" },
-      progressBarOption: {
-        rate: "85%",
-        colorTo: "#339933",
-        colorFrom: "whitesmoke",
-      },
-    },
-  ];
   return (
     <div className="App">
-      <Header />
-      <Carousel transition={1000} width="80%" arrowLocation="mid-side">
-        <Image src="https://picsum.photos/600/600/?random" />
-        <Image src="https://picsum.photos/800/600/?random" />
-        <Item title="1" />
-        <Item title="2" />
+      <Header
+        headerBackgroundColor="white"
+        sideBarOption={sideBarOptionDefault}
+        logoOption={logoOptionDefault}
+        channels={channelOptionDefault}
+      />
+      <Carousel
+        id="TOP"
+        isArrowShow={false}
+        transition={100}
+        width="60%"
+        arrowLocation="mid-side"
+      >
+        <Image noShowHead={true} src="common/banner.png" />
       </Carousel>
-      <Intro />
-      <TechStackList techStackList={techStacksDefault} />
-      <TechStackInput />
-      <Experience />
-      <Gallery>
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-      </Gallery>
-      <Masonry>
-        <Image src="https://picsum.photos/600/600/?random" />
-        <Image src="https://picsum.photos/600/1100/?random" />
-        <Image src="https://picsum.photos/300/400/?random" />
-        <Image src="https://picsum.photos/600/400/?random" />
-        <Image src="https://picsum.photos/600/900/?random" />
-        <Image src="https://picsum.photos/600/300/?random" />
-        <Image src="https://picsum.photos/400/100/?random" />
-        <Image src="https://picsum.photos/600/600/?random" />
-        <Image src="https://picsum.photos/600/800/?random" />
-        <Image src="https://picsum.photos/600/1000/?random" />
-        <Image src="https://picsum.photos/600/500/?random" />
-        <Image src="https://picsum.photos/500/600/?random" />
-      </Masonry>
-      <CardWrap>
-        <Card width="20em" height="20em" hover="down">
-          This is Card component
-        </Card>
-        <Card width="20em" height="20em" shape="round-square" hover="zoom">
-          You can use this components anyware
-        </Card>
-        <Card width="20em" height="20em" shape="round" hover="up">
-          See official documentation for details
-        </Card>
-      </CardWrap>
-      <Contact />
+      <SizeAdjustment id="INTRO">
+        <h3>Intro</h3>
+        <Intro backgroundColor="white" introOption={introOptionDefault} />
+      </SizeAdjustment>
+      <TeckStackWrap id="SKILL">
+        <h3>Skill</h3>
+        <TechStackList techStackList={techStacksDefault} />
+      </TeckStackWrap>
+      <SizeAdjustment id="EXPERIENCE">
+        <Experience
+          theme="vertical"
+          verticalOption={{ shape: "round-square" }}
+          historyList={historyListDefault}
+        />
+      </SizeAdjustment>
+      <GalleryWrap id="PROJECT">
+        <h2>Project</h2>
+        <Gallery col={2} padding="0em 10em">
+          <Item
+            redirectURL="https://dongurami.co.kr/"
+            imgURL="common/circle.png"
+            title="동그라미"
+            description="교내 동아리 커뮤니티 플랫폼 서비스"
+          />
+          <Item
+            redirectURL="https://www.npmjs.com/package/dev-portfolio"
+            imgURL="common/dev-portfolio.png"
+            title="dev-portfolio"
+            description="포트폴리오 개발 오픈소스 라이브러리"
+          />
+          <Item
+            redirectURL="https://kitty-collector.vercel.app/"
+            imgURL="common/kittyMain.png"
+            title="키티 컬렉터"
+            description="화살표에 맞는 고양이를 선택하는 게임"
+          />
+        </Gallery>
+      </GalleryWrap>
+      <SizeAdjustment id="PRESENTATION">
+        <h3>Presentation</h3>
+        <Masonry>
+          <Image
+            zoomWhenHover={true}
+            redirectURL="https://youtu.be/6FHMMk-gs0M"
+            src="common/tecktalk/1.png"
+            subhead=""
+            head="브라우저 저장소의 차이"
+          />
+          <Image
+            zoomWhenHover={true}
+            redirectURL="https://youtu.be/7SGSN5h-u_w"
+            src="common/tecktalk/2.png"
+            subhead=""
+            head="git VS github"
+          />
+          <Image
+            zoomWhenHover={true}
+            redirectURL="https://youtu.be/fNrm1JsktfQ"
+            src="common/tecktalk/3.png"
+            subhead=""
+            head="dfs와 bfs"
+          />
+          <Image
+            zoomWhenHover={true}
+            redirectURL="https://youtu.be/g6L5rAlplig"
+            src="common/tecktalk/4.png"
+            subhead=""
+            head="http 상태코드"
+          />
+          <Image
+            zoomWhenHover={true}
+            redirectURL="https://youtu.be/7phRfXdvb4M"
+            src="common/tecktalk/5.png"
+            subhead=""
+            head="React"
+          />
+          <Image
+            zoomWhenHover={true}
+            redirectURL="https://youtu.be/Ghsow9H2BWg"
+            src="common/tecktalk/6.png"
+            subhead=""
+            head="var, let, const"
+          />
+          <Image
+            zoomWhenHover={true}
+            redirectURL="https://youtu.be/yoHZVmEbnCo"
+            src="common/tecktalk/7.png"
+            subhead=""
+            head="css와 scss"
+          />
+        </Masonry>
+      </SizeAdjustment>
+      <Contact
+        id="CONTACT"
+        backgroundColor="#c3c7de33"
+        contactOption={contactOptionDefault}
+      />
     </div>
   );
 }
 
 export default App;
 
-const CardWrap = styled.div`
-  display: flex;
-  padding: 3em;
-  justify-content: space-around;
+const SizeAdjustment = styled.div`
+  padding: 10em;
+  h3 {
+    margin: 0;
+    font-size: 40px;
+    padding: 10px 0;
+    border-bottom: 1px solid;
+  }
+`;
+
+const TeckStackWrap = styled.div`
+  padding: 10em;
+  h3 {
+    font-size: 40px;
+    padding: 10px 0;
+    border-bottom: 1px solid;
+  }
+`;
+
+const GalleryWrap = styled.div`
+  padding: 10em;
+  h2 {
+    font-size: 35px;
+    padding: 10px 10px 20px 10px;
+    border-bottom: 1px solid;
+  }
 `;
---
+++ app/src/defaultProps/CommonProps.ts
@@ -0,0 +1,148 @@
+export const techStacksDefault = [
+  {
+    nameOption: { name: "HTML-5", size: "50px", iconColor: "red" },
+    progressBarOption: {
+      rate: "95%",
+      colorTo: "#C3C7DE",
+      colorFrom: "#C3C7DE",
+      height: "40px",
+    },
+  },
+  {
+    nameOption: { name: "CSS-3", size: "40px", iconColor: "#339933" },
+    progressBarOption: {
+      rate: "99%",
+      colorTo: "#544F7C",
+      colorFrom: "#544F7C",
+      height: "40px",
+    },
+  },
+  {
+    nameOption: { name: "Javascript", size: "40px", iconColor: "#E2D784" },
+    progressBarOption: {
+      rate: "90%",
+      colorTo: "#C3C7DE",
+      colorFrom: "#C3C7DE",
+      height: "40px",
+    },
+  },
+  {
+    nameOption: { name: "Typescript", size: "40px", iconColor: "#339933" },
+    progressBarOption: {
+      rate: "80%",
+      colorTo: "#544F7C",
+      colorFrom: "#544F7C",
+      height: "40px",
+    },
+  },
+  {
+    nameOption: { name: "React", size: "40px", iconColor: "#339933" },
+    progressBarOption: {
+      rate: "85%",
+      colorTo: "#544F7C",
+      colorFrom: "#544F7C",
+      height: "40px",
+    },
+  },
+  {
+    nameOption: { name: "Nextjs", size: "40px", iconColor: "#339933" },
+    progressBarOption: {
+      rate: "80%",
+      colorTo: "#C3C7DE",
+      colorFrom: "#C3C7DE",
+      height: "40px",
+    },
+  },
+  {
+    nameOption: { name: "Redux", size: "40px", iconColor: "#339933" },
+    progressBarOption: {
+      rate: "40%",
+      colorTo: "#C3C7DE",
+      colorFrom: "#C3C7DE",
+      height: "40px",
+    },
+  },
+  {
+    nameOption: { name: "Sass", size: "40px", iconColor: "#339933" },
+    progressBarOption: {
+      rate: "90%",
+      colorTo: "#544F7C",
+      colorFrom: "#544F7C",
+      height: "40px",
+    },
+  },
+];
+
+export const introOptionDefault = {
+  title: "",
+  shortIntro: "세상에 긍정적인 변화를 일으킬 개발자 입니다.",
+  description:
+    "안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n안녕하세요, 주니어 웹 프론트앤드 개발자 심서현입니다.\n\n잘부탁드립니다^^",
+};
+
+export const historyListDefault = [
+  {
+    startDate: "2021.01",
+    endDate: "2021.02",
+    title: "미식의 시대",
+    des: "스타트업 인턴십\n\n약 6주간 인턴십을 진행했습니다.\n주로 SQL, QA업무를 진행했습니다.",
+  },
+  {
+    startDate: "2021.05",
+    endDate: "2022.01",
+    title: "모던 애자일",
+    des: "서비스 개발\n\n프론트앤드 개발자로 활동하며 서비스를 개발했습니다.\n애자일 협업방식을 따르며 체계적인 개발을 진행했습니다.",
+  },
+  {
+    startDate: "2022.08",
+    endDate: "현재진행형",
+    title: "취업준비중",
+    des: "취업해야지 이제~",
+  },
+];
+
+const channelOptionDefault = [
+  {
+    name: "github",
+    redirectUrl: "https://github.com/seohyunsim",
+    color: "black",
+    size: "23px",
+  },
+  {
+    name: "velog",
+    redirectUrl: "https://velog.io/@seohyunsim",
+    color: "#20C997",
+    size: "23px",
+  },
+  {
+    name: "instagram",
+    color: "#E1406C",
+    size: "23px",
+  },
+];
+
+export const contactOptionDefault = {
+  title: "저에 대해 더 궁금하신가요?",
+  subTitle: "주저하지 말고 지금 바로 📧📲",
+  email: "ssh4563@naver.com",
+  buttonText: "Come here to send mail !",
+  channels: channelOptionDefault,
+  aboutMeInfos: [
+    {
+      title: "Residence",
+      description: "Seoul, Republic of Korea",
+    },
+    {
+      title: "Birth",
+      description: "2000. 11. 20",
+    },
+    {
+      title: "MBTI",
+      description: "ESFP || ENFP",
+    },
+    {
+      title: "Phone",
+      description: "010 9572 3357",
+    },
+  ],
+};
