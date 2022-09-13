9b10ba1d22c9bd77512976c4a7aaff19be8f3a63
woorim960 <woorimprog@gmail.com>
2022-09-13 22:02:26 +0900

    [Docs] add TypeDoc comments at all of components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,4 @@
+/* This is imported components of dev-portfolio library */
 import {
   Header,
   TechStackList,
@@ -12,6 +13,8 @@ import {
   TechStackInput,
   VisitorComment,
 } from "dev-portfolio";
+
+import styled from "styled-components";
 import {
   CardInstruction,
   Introduction,
@@ -19,10 +22,16 @@ import {
   DisplayPortfolioInstruction,
   VisitorCounterInstruction,
 } from "./common";
-import "./App.css";
 import useComment from "./hooks/useComment";
+import "./App.css";
 
 function App() {
+  /**
+   * If you want to view README.md of 'dev-portfolio', go to the link below.
+   * {@link https://github.com/modern-agile-team/dev-portfolio#readme}
+   */
+
+  /* These are variables and handler functions used in VisitorComment component. */
   const {
     comment,
     commentList,
@@ -33,21 +42,105 @@ function App() {
     handleChangePassword,
     handleCreateComment,
   } = useComment();
+
   return (
+    /**
+     * The 'className' in the <div> tag surrounding the components of 'dev-portfolio' must be 'App'.
+     * Only then can the SideBar in the <Header> component recognize id props and automatically assign all components into the SideBar.
+     */
     <div className="App">
-      <Header id="Header" />
-      <Introduction />
+      {/**
+       * @component Header
+       * {@link https://github.com/modern-agile-team/dev-portfolio#header}
+       */}
+      <Header
+        id="Header"
+        logoOption={{
+          redirectUrl: "/",
+          title: "dev-portfolio",
+          logoMargin: "0 20px 0 20px",
+          logoWidth: "60px",
+          titleColor: "black",
+          titleSize: "30px",
+        }}
+        channels={[
+          {
+            name: "github",
+            redirectUrl: "https://",
+            color: "#181717BB",
+            size: "30px",
+          },
+          {
+            name: "facebook",
+            redirectUrl: "https://",
+            color: "#1877F2BB",
+            size: "30px",
+          },
+        ]}
+        sideBarOption={{
+          mainTitle: "dev-portfolio",
+          iconSize: "30px",
+          iconColor: "#181717BB",
+          iconMargin: "0px 20px 0px 20px",
+        }}
+      />
+
+      {/**
+       * Just introduction for dev-portfolio-app.
+       *
+       * If you want view internal of Introduction,
+       * go to the './src/common/instruction/Introduction.tsx'
+       */}
+      <Introduction id="Manual Introduction" />
+
+      {/**
+       * @component Intro
+       * {@link https://github.com/modern-agile-team/dev-portfolio#intro}
+       */}
       <Intro id="Intro" />
-      <TechStackList id="TechStackList" />
-      <TeckstackInputInstruction />
+
+      {/**
+       * @component TechStackList
+       * {@link https://github.com/modern-agile-team/dev-portfolio#techstacklist}
+       */}
+      <TechStackListTitle id="TechStackList">
+        Tech Stack List
+      </TechStackListTitle>
+      <TechStackList />
+
+      {/**
+       * TechStackInput used only to find the logoName value in the TechStackList.
+       * @component TechStackInput
+       * {@link none}
+       *
+       * If you want view internal of TeckstackInputInstruction,
+       * go to the './src/common/instruction/TeckstackInputInstruction.tsx'
+       */}
+      <TeckstackInputInstruction id="TechStackInput" />
       <TechStackInput />
-      <Experience id="Experience" />
+
+      {/**
+       * Just introduction for Carousel, Gallery and Masonry.
+       *
+       * If you want view internal of DisplayPortfolioInstruction,
+       * go to the './src/common/instruction/DisplayPortfolioInstruction.tsx'
+       */}
       <DisplayPortfolioInstruction />
+
+      {/**
+       * @component Carousel
+       * {@link https://github.com/modern-agile-team/dev-portfolio#carousel}
+       */}
       <Carousel id="Carousel">
         <Item title="1" />
         <Item title="2" />
         <Item title="3" />
       </Carousel>
+
+      {/**
+       * @component Gallery
+       * {@link https://github.com/modern-agile-team/dev-portfolio#gallery}
+       */}
       <Gallery id="Gallery">
         <Item />
         <Item />
@@ -59,6 +152,11 @@ function App() {
         <Item />
         <Item />
       </Gallery>
+
+      {/**
+       * @component Masonry
+       * {@link https://github.com/modern-agile-team/dev-portfolio#masonry}
+       */}
       <Masonry id="Masonry">
         <Image src="https://picsum.photos/600/600/?random" />
         <Image src="https://picsum.photos/600/900/?random" />
@@ -74,9 +172,24 @@ function App() {
         <Image src="https://picsum.photos/500/600/?random" />
         <Image src="https://picsum.photos/700/600/?random" />
       </Masonry>
-      <VisitorCounterInstruction />
+
+      {/**
+       * @component VisitorCounter
+       * {@link https://github.com/modern-agile-team/dev-portfolio#visitorcounter}
+       *
+       * If you want view Card component,
+       * go to the './src/common/instruction/VisitorCounterInstruction.tsx'
+       */}
+      <VisitorCounterInstruction id="VisitorCounter" />
+
+      {/**
+       * @component VisitorComment
+       * {@link https://github.com/modern-agile-team/dev-portfolio#visitorcomment}
+       */}
+      <VisitorCommentTitle id="VisitorComment">
+        Visitor Comments
+      </VisitorCommentTitle>
       <VisitorComment
-        id="VisitorComment"
         handleChangeDescription={handleChangeDescription}
         handleChangeNickname={handleChangeNickname}
         handleChangePassword={handleChangePassword}
@@ -86,10 +199,61 @@ function App() {
         password={password}
         commentList={commentList}
       />
-      <CardInstruction />
-      <Contact id="Contact" />
+
+      {/**
+       * @component Card
+       * {@link https://github.com/modern-agile-team/dev-portfolio#card}
+       *
+       * If you want view Card component,
+       * go to the './src/common/instruction/CardInstruction.tsx'
+       */}
+      <CardInstruction id="Card" />
+
+      {/**
+       * @component Experience
+       * {@link https://github.com/modern-agile-team/dev-portfolio#experience}
+       */}
+      <Experience id="Experience" theme="basic" />
+
+      {/**
+       * @component Contact
+       * {@link https://github.com/modern-agile-team/dev-portfolio#contact}
+       */}
+      <ContactTitle id="Contact">Contact</ContactTitle>
+      <Contact />
     </div>
   );
 }
 
 export default App;
+
+/**
+ * Just styled component for TechStackList's title
+ *
+ * If you don't need this, delete both TechStackListTitle component and the style components below.
+ */
+const TechStackListTitle = styled.h1`
+  margin: 1em 1em;
+  padding-bottom: 15px;
+  border-bottom: 1px solid;
+`;
+
+/**
+ * Just styled component for VisitorComment's title
+ *
+ * If you don't need this, delete both VisitorCommentTitle component and the style components below.
+ */
+const VisitorCommentTitle = styled.h1`
+  margin: 1em 1em 0 1em;
+  padding-bottom: 15px;
+`;
+
+/**
+ * Just styled component for Contact's title
+ *
+ * If you don't need this, delete both ContactTitle component and the style components below.
+ */
+const ContactTitle = styled.h1`
+  margin: 0;
+  padding: 2em 1em 0 1em;
+`;
