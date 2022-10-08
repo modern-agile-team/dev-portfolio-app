a15ca95d246f7d4a28fd7a5c5b54d5917bf38883
woorim960 <woorimprog@gmail.com>
2022-10-09 01:45:11 +0900

    fix(index.tsx): Fix error that render twice in a row #3

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,11 +6,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
